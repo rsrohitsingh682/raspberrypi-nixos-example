9d38eab93d350d8856bfb7a5327613cd0d99b9c7
Rohit Singh <rsrohitsingh682@gmail.com>
2024-10-04 15:29:39 +0530

    Update configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -4,20 +4,9 @@
   services.openssh.enable = true;
   networking.hostName = "pi";
   users = {
-    users.myUsername = {
-      password = "myPassword";
+    users.rohit = {
       isNormalUser = true;
       extraGroups = [ "wheel" ];
-    };
-  };
-  networking = {
-    interfaces."wlan0".useDHCP = true;
-    wireless = {
-      interfaces = [ "wlan0" ];
-      enable = true;
-      networks = {
-        networkSSID.psk = "password";
-      };
     };
   };
 }
